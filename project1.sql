712be1611ac4b0976cde732ec8d4981227019ef9
Sohailshaikh5656 <134220674+Sohailshaikh5656@users.noreply.github.com>
2025-06-16 17:29:14 +0530

    Add files via upload
    Data base for this Project

--- project1.sql
+++ project1.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2023 at 02:46 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Host: 127.0.0.1:3307
+-- Generation Time: Jun 16, 2025 at 01:58 PM
+-- Server version: 10.6.19-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,6 +48,8 @@ INSERT INTO `admin` (`id`, `email`, `password`) VALUES
 
 CREATE TABLE `bookings` (
   `id` int(11) NOT NULL,
+  `clinic_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `contact` bigint(10) NOT NULL,
@@ -60,10 +62,33 @@ CREATE TABLE `bookings` (
 -- Dumping data for table `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `name`, `email`, `contact`, `description`, `timeslots`, `date`) VALUES
-(1, 'sohail', 'shaikhsohail1131@gmail.com', 0, 'back pain', '13:20PM-13:30PM', '2023-11-18'),
-(2, 'sohail', 'shaikhsohail1131@gmail.com', 0, 'wrist pain', '11:20AM-11:30AM', '2023-11-18'),
-(3, 'sohail', 'shaikhsohail1131@gmail.com', 6354621866, 'fever', '12:20PM-12:30PM', '2023-11-18');
+INSERT INTO `bookings` (`id`, `clinic_id`, `user_id`, `name`, `email`, `contact`, `description`, `timeslots`, `date`) VALUES
+(1, 0, 0, 'sohail', 'shaikhsohail1131@gmail.com', 0, 'back pain', '13:20PM-13:30PM', '2023-11-18'),
+(2, 0, 0, 'sohail', 'shaikhsohail1131@gmail.com', 0, 'wrist pain', '11:20AM-11:30AM', '2023-11-18'),
+(3, 0, 0, 'sohail', 'shaikhsohail1131@gmail.com', 6354621866, 'fever', '12:20PM-12:30PM', '2023-11-18'),
+(4, 1, 1, 'sohail shaikh', 'shaikhsohail1131@gmail.com', 6354621866, 'Normal Fever From Last Two days and Chest Pain.', '10:30AM-10:40AM', '2025-06-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `state_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`, `state_id`) VALUES
+(1, 'Ahmedabad', 1),
+(2, 'Surat', 1),
+(3, 'Chandigard', 2),
+(4, 'Amritsar', 2);
 
 -- --------------------------------------------------------
 
@@ -72,25 +97,91 @@ INSERT INTO `bookings` (`id`, `name`, `email`, `contact`, `description`, `timesl
 --
 
 CREATE TABLE `clinic` (
-  `id` int(10) NOT NULL,
-  `clinic_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `contact` bigint(10) NOT NULL,
-  `password` varchar(16) NOT NULL,
-  `address` text NOT NULL,
-  `owner_name` varchar(30) NOT NULL,
-  `owner_email` varchar(50) NOT NULL,
-  `owner_contact` bigint(10) NOT NULL,
-  `approval` tinyint(1) NOT NULL
+  `id` int(11) NOT NULL,
+  `clinic_name` varchar(255) NOT NULL,
+  `clinic_type` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `owner_name` varchar(255) NOT NULL,
+  `owner_email` varchar(255) NOT NULL,
+  `owner_contact` varchar(255) NOT NULL,
+  `approval` tinyint(1) NOT NULL DEFAULT 0,
+  `profile_image` varchar(255) NOT NULL,
+  `poster_image` varchar(255) NOT NULL,
+  `doctor_image` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `clinic`
 --
 
-INSERT INTO `clinic` (`id`, `clinic_name`, `email`, `contact`, `password`, `address`, `owner_name`, `owner_email`, `owner_contact`, `approval`) VALUES
-(6, '', 'shaikhsohail1131@gmail.com', 6354621866, '1212', '', 'Sohail', 'shaikhsohail11131@gmail.com', 0, 1),
-(7, 'xyz', 'xyz@gmail.com', 9054621866, '1212', 'behrampura', 'Sohail', 'shaikhsohail11131@gmail.com', 6354621866, 1);
+INSERT INTO `clinic` (`id`, `clinic_name`, `clinic_type`, `password`, `email`, `contact`, `address`, `country`, `state`, `owner_name`, `owner_email`, `owner_contact`, `approval`, `profile_image`, `poster_image`, `doctor_image`, `city`) VALUES
+(1, 'shifa Clinic', 'Cardiology', '1212', 'shifa@gmail.com', '6354621866', '501 Dauphin St Apt A', 'India', 'Gujarat', 'Mumtazz', 'mumtazz@gmail.com', '6354621866', 1, '', '', '', 'Ahmedabad');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clinic_profile`
+--
+
+CREATE TABLE `clinic_profile` (
+  `id` int(11) NOT NULL,
+  `clinic_id` int(11) NOT NULL,
+  `profile_image` varchar(255) NOT NULL,
+  `poster_image` varchar(255) NOT NULL,
+  `doctor_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clinic_profile`
+--
+
+INSERT INTO `clinic_profile` (`id`, `clinic_id`, `profile_image`, `poster_image`, `doctor_image`) VALUES
+(1, 1, 'img1_klinicjpg.jpg', 'klinic_poster.jpg', 'doctor_image.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'India'),
+(2, 'USA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `name`, `country_id`) VALUES
+(1, 'Gujarat', 1),
+(2, 'Punjab', 1);
 
 -- --------------------------------------------------------
 
@@ -134,11 +225,36 @@ ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `state_id` (`state_id`);
+
+--
 -- Indexes for table `clinic`
 --
 ALTER TABLE `clinic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clinic_profile`
+--
+ALTER TABLE `clinic_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD KEY `country_id` (`country_id`);
 
 --
 -- Indexes for table `user`
@@ -161,19 +277,59 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `clinic`
 --
 ALTER TABLE `clinic`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `clinic_profile`
+--
+ALTER TABLE `clinic_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cities`
+--
+ALTER TABLE `cities`
+  ADD CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`);
+
+--
+-- Constraints for table `states`
+--
+ALTER TABLE `states`
+  ADD CONSTRAINT `states_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
